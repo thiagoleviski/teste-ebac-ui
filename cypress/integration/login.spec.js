e816f7f1ca8c47229e5c86a52eb816a538ece7ad
Thiago Leviski <thiago_leviski@yahoo.com.br>
2022-10-20 07:53:36 -0300

    Testes adicionados do curso EBAC

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,19 +1,25 @@
 /// <reference types = "cypress" />
 
 context('Funcionalidade Login', () => {
-    
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.page-title').should('contain','Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, teste_aluno20 (não é teste_aluno20? Sair)')
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste_aluno20 (não é teste_aluno20? Sair)')
     });
 
-    it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () =>{
-        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
+    it('Deve exibir uma mensagem de erro ao inserir usuario inválido', () => {
         cy.get('#username').type('ebax@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -23,7 +29,6 @@ context('Funcionalidade Login', () => {
     });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
