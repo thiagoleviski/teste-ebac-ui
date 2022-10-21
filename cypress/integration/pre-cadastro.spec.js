eb7f3987557122e2241c0db5b72c28aa1830fa1e
Thiago Leviski <thiago_leviski@yahoo.com.br>
2022-10-21 20:30:36 -0300

    Novos testes adicionados

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -7,7 +7,7 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
     });
 
-    it('Deve completar o pré cadastro com sucesso', () => {
+    it('Deve completar o pré cadastro com sucesso e preencher o endereço de faturamento', () => {
         let nomeFaker = faker.name.firstName()
         let sobrenomeFaker = faker.name.lastName()
         let emailFaker = faker.internet.email(nomeFaker)
@@ -22,5 +22,27 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
-    });
-});
+
+// A partir daqui adicionei o preenchimento do endereço de faturamento
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        cy.get(':nth-child(1) > .title > .edit').click()
+
+        cy.get('#select2-billing_country-container').click()
+        cy.get('[class="select2-results__options"]').contains('li','Brasil').click()
+        cy.get('#billing_address_1').type('Rua X')
+        cy.get('#billing_city').type('Campinas')
+        cy.get('#select2-billing_state-container').click()
+        cy.get('[class="select2-results__options"]').contains('li','São Paulo').click()
+        cy.get('#billing_postcode').type('13000-000')
+        cy.get('#billing_phone').type(19999999999)
+        cy.get('.button').click()
+
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+        cy.get(':nth-child(1) > address').should('contain',`${nomeFaker} ${sobrenomeFaker}`)
+        cy.get(':nth-child(1) > address').should('contain',`Rua X`)
+        cy.get(':nth-child(1) > address').should('contain',`Campinas`)
+        cy.get(':nth-child(1) > address').should('contain',`São Paulo`)
+        cy.get(':nth-child(1) > address').should('contain',`13000-000`)
+    })
+})
